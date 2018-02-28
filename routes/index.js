dd77420b3ef4d31d1642d13d9f38883712d77746
Bob Townsend <bt.codes.it@gmail.com>
2018-02-28 15:02:51 -0700

    End of day 2

--- routes/index.js
+++ routes/index.js
@@ -25,34 +25,50 @@ router.get("/:username", (req, res) => {
     .set("Accept", "application/json")
     .end((err, response) => {
       if (err) {
-        res.json({
-          confirmation: "fail",
-          message: err.message
-        });
+        const data = {
+          message: err.message || "Check your spelling"
+        };
+        res.render("error", data);
         return;
       }
       res.render("index", response.body);
-      //
     });
 });
 
-// /*  This route render json data */
-// router.get('/json', (req, res) => {
-// 	res.json({
-// 		confirmation: 'success',
-// 		app: process.env.TURBO_APP_ID,
-// 		data: 'this is a sample json route.'
-// 	})
-// })
+router.get("/:username/:postcode", (req, res) => {
+  const username = req.params.username;
+  const postcode = req.params.postcode;
 
-// /*  This route sends text back as plain text. */
-// router.get('/send', function(req, res){
-// 	res.send('This is the Send Route')
-// })
+  const instagramAPI = "https://www.instagram.com/" + username + "/?__a=1";
 
-// /*  This route redirects requests to Turbo360. */
-// router.get('/redirect', function(req, res){
-// 	res.redirect('https://www.turbo360.co/landing')
-// })
+  superagent
+    .get(instagramAPI)
+    .query(null)
+    .set("Accept", "application/json")
+    .end((err, response) => {
+      if (err) {
+        const data = {
+          message: err.message || "Check your spelling"
+        };
+        res.render("error", data);
+        return;
+      }
+
+      const posts = response.body.user.media.nodes;
+      let selectedPost = null;
+      for (let i = 0; i < posts.length; i++) {
+        const post = posts[i];
+        if (post.code == postcode) selectedPost = post;
+        break;
+      }
+
+      if (selectedPost == null) {
+        res.render("error", { message: "Post not found" });
+        return;
+      }
+
+      res.render("post", selectedPost);
+    });
+});
 
 module.exports = router;
